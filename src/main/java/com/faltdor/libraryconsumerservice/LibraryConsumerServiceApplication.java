288b3d4cb200f80ca7fa7e5cc9d80fccccbc0acd
Andres Cardona <faltdor@gmail.com>
2022-08-09 21:16:35 -0500

    first commit

---
+++ src/main/java/com/faltdor/libraryconsumerservice/LibraryConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.faltdor.libraryconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryConsumerServiceApplication.class, args);
+	}
+
+}
